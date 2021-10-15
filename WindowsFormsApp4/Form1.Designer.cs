d2c4cdd123785343d0b13486489a5b3d89206c54
Artyom Golenishchev <48377936+heywannafunk@users.noreply.github.com>
2021-10-15 20:58:30 +0300

    Add files via upload
    - segments
    - shifts 4 polygons & points
    - rotations 4 segments
    - left or right

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -31,6 +31,9 @@ namespace WindowsFormsApp4
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -42,10 +45,12 @@ namespace WindowsFormsApp4
             this.panel2 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.radioPolygon = new System.Windows.Forms.RadioButton();
+            this.radioRectangle = new System.Windows.Forms.RadioButton();
             this.radioSegment = new System.Windows.Forms.RadioButton();
             this.radioPoint = new System.Windows.Forms.RadioButton();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -57,10 +62,9 @@ namespace WindowsFormsApp4
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.panel5);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(17, 16);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1033, 523);
+            this.panel1.Size = new System.Drawing.Size(775, 425);
             this.panel1.TabIndex = 0;
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
@@ -68,6 +72,11 @@ namespace WindowsFormsApp4
             // 
             this.panel5.BackColor = System.Drawing.SystemColors.Control;
             this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Controls.Add(this.textBox5);
+            this.panel5.Controls.Add(this.button4);
+            this.panel5.Controls.Add(this.textBox4);
+            this.panel5.Controls.Add(this.button3);
             this.panel5.Controls.Add(this.button2);
             this.panel5.Controls.Add(this.label4);
             this.panel5.Controls.Add(this.label3);
@@ -76,86 +85,111 @@ namespace WindowsFormsApp4
             this.panel5.Controls.Add(this.label2);
             this.panel5.Controls.Add(this.textBox1);
             this.panel5.Controls.Add(this.label1);
-            this.panel5.Enabled = false;
-            this.panel5.Location = new System.Drawing.Point(5, 411);
-            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel5.Location = new System.Drawing.Point(4, 334);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(1021, 105);
+            this.panel5.Size = new System.Drawing.Size(766, 86);
             this.panel5.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(202, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Rotate";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.Location = new System.Drawing.Point(283, 5);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(202, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Point pos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(11, 70);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(8, 57);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
             this.button2.Text = "Shift";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(156, 78);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(117, 63);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(27, 17);
+            this.label4.Size = new System.Drawing.Size(21, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "dy:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(156, 46);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(117, 37);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 17);
+            this.label3.Size = new System.Drawing.Size(21, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "dx:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(192, 74);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Location = new System.Drawing.Point(144, 60);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(68, 22);
+            this.textBox3.Size = new System.Drawing.Size(52, 20);
             this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(192, 42);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(144, 34);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(68, 22);
+            this.textBox2.Size = new System.Drawing.Size(52, 20);
             this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 46);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(5, 37);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 17);
+            this.label2.Size = new System.Drawing.Size(82, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Placement shift:";
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(128, 5);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(96, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(5, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Current primitive:";
             // 
@@ -165,22 +199,21 @@ namespace WindowsFormsApp4
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.panel3);
-            this.panel2.Controls.Add(this.radioPolygon);
+            this.panel2.Controls.Add(this.radioRectangle);
             this.panel2.Controls.Add(this.radioSegment);
             this.panel2.Controls.Add(this.radioPoint);
             this.panel2.Controls.Add(this.panel4);
-            this.panel2.Location = new System.Drawing.Point(924, 5);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(693, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(102, 182);
+            this.panel2.Size = new System.Drawing.Size(77, 148);
             this.panel2.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(4, 153);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(3, 124);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.Size = new System.Drawing.Size(69, 19);
             this.button1.TabIndex = 5;
             this.button1.Text = "Clear scene";
             this.button1.UseVisualStyleBackColor = true;
@@ -190,32 +223,29 @@ namespace WindowsFormsApp4
             // 
             this.panel3.BackColor = System.Drawing.Color.Black;
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel3.Location = new System.Drawing.Point(4, 4);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Location = new System.Drawing.Point(3, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(63, 36);
+            this.panel3.Size = new System.Drawing.Size(48, 30);
             this.panel3.TabIndex = 3;
             this.panel3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseDown);
             // 
             // radioRectangle
             // 
-            this.radioPolygon.AutoSize = true;
-            this.radioPolygon.Location = new System.Drawing.Point(3, 123);
-            this.radioPolygon.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.radioPolygon.Name = "radioRectangle";
-            this.radioPolygon.Size = new System.Drawing.Size(80, 21);
-            this.radioPolygon.TabIndex = 2;
-            this.radioPolygon.TabStop = true;
-            this.radioPolygon.Text = "Polygon";
-            this.radioPolygon.UseVisualStyleBackColor = true;
+            this.radioRectangle.AutoSize = true;
+            this.radioRectangle.Location = new System.Drawing.Point(2, 100);
+            this.radioRectangle.Name = "radioRectangle";
+            this.radioRectangle.Size = new System.Drawing.Size(63, 17);
+            this.radioRectangle.TabIndex = 2;
+            this.radioRectangle.TabStop = true;
+            this.radioRectangle.Text = "Polygon";
+            this.radioRectangle.UseVisualStyleBackColor = true;
             // 
             // radioSegment
             // 
             this.radioSegment.AutoSize = true;
-            this.radioSegment.Location = new System.Drawing.Point(3, 95);
-            this.radioSegment.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioSegment.Location = new System.Drawing.Point(2, 77);
             this.radioSegment.Name = "radioSegment";
-            this.radioSegment.Size = new System.Drawing.Size(85, 21);
+            this.radioSegment.Size = new System.Drawing.Size(67, 17);
             this.radioSegment.TabIndex = 1;
             this.radioSegment.TabStop = true;
             this.radioSegment.Text = "Segment";
@@ -224,10 +254,9 @@ namespace WindowsFormsApp4
             // radioPoint
             // 
             this.radioPoint.AutoSize = true;
-            this.radioPoint.Location = new System.Drawing.Point(3, 66);
-            this.radioPoint.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioPoint.Location = new System.Drawing.Point(2, 54);
             this.radioPoint.Name = "radioPoint";
-            this.radioPoint.Size = new System.Drawing.Size(61, 21);
+            this.radioPoint.Size = new System.Drawing.Size(49, 17);
             this.radioPoint.TabIndex = 0;
             this.radioPoint.TabStop = true;
             this.radioPoint.Text = "Point";
@@ -237,20 +266,35 @@ namespace WindowsFormsApp4
             // 
             this.panel4.BackColor = System.Drawing.Color.White;
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel4.Location = new System.Drawing.Point(32, 22);
-            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel4.Location = new System.Drawing.Point(24, 18);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(63, 36);
+            this.panel4.Size = new System.Drawing.Size(48, 30);
             this.panel4.TabIndex = 4;
             this.panel4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseDown);
             // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(331, 34);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(52, 20);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(283, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "degrees:";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -268,7 +312,7 @@ namespace WindowsFormsApp4
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.RadioButton radioPolygon;
+        private System.Windows.Forms.RadioButton radioRectangle;
         private System.Windows.Forms.RadioButton radioSegment;
         private System.Windows.Forms.RadioButton radioPoint;
         private System.Windows.Forms.Panel panel4;
@@ -282,6 +326,11 @@ namespace WindowsFormsApp4
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
